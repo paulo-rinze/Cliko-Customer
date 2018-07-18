52d86ffec28600116e8b7058c32dcaf2b9c8a468
Paulo Rinze <paulorinze@gmail.com>
2018-07-18 12:11:24 +0200

    Cliko CustomerApp Upload
    First Upload Customer App

---
+++ www/js/config.js
@@ -0,0 +1,7 @@
+var krms_config ={			
+	'ApiUrl':"https://www.cliko.co.mz/mobileapp/api",	
+	'DialogDefaultTitle':"cliko",	
+	'pushNotificationSenderid':"1057657492528",	
+	'facebookAppId':"1592399797464675", 
+	'APIHasKey':"d19fc63aecb6ac4821c5fc21bdced35f"
+};

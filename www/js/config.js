45751c98e95c7bf5fc71d5488210c32d51d84d41
Paulo Rinze <paulorinze@gmail.com>
2018-08-08 14:43:40 +0200

    up

--- www/js/config.js
+++ www/js/config.js
@@ -1,7 +1,7 @@
 var krms_config ={			
 	'ApiUrl':"https://www.cliko.co.mz/mobileapp/api",	
 	'DialogDefaultTitle':"cliko",	
-	'pushNotificationSenderid':"1057657492528",	
+	'pushNotificationSenderid':"387508101893",	
 	'facebookAppId':"1301128676689561", 
-	'APIHasKey':"d19fc63aecb6ac4821c5fc21bdced35f"
+	'APIHasKey':"customerApp"
 };
